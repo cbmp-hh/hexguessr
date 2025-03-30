94a1787c6a5cd78392996afcd50a18c4876b1167
cbmp hh <111956671+cbmp-hh@users.noreply.github.com>
2025-03-30 19:23:52 +0800

    Fix colour display bug

--- hexguessr/main.js
+++ hexguessr/main.js
@@ -4,7 +4,7 @@ function colourgen() {
     var b = Math.floor(Math.random() * 255);
     var colour = r.toString(16) + g.toString(16) + b.toString(16);
     var brightness = r + g + b;
-    document.documentElement.style.setProperty("--backgroundColour", "#" + colour);
+    document.documentElement.style.setProperty("--backgroundColour", "rgb(" + r + ", " + g + ", " + b + ")");
 
     if (brightness > 381) {
         document.documentElement.style.setProperty("--colour", "black");
