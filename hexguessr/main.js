e653fad9693d693b88c553bbabe4f4b1aacd3aeb
cbmp hh <111956671+cbmp-hh@users.noreply.github.com>
2025-04-08 23:49:28 +0800

    Add display of results

--- hexguessr/main.js
+++ hexguessr/main.js
@@ -33,7 +33,18 @@ function load() {
 }
 
 function submit() {
-    console.log(colourDifference(guess.value, colour));
+    document.documentElement.style.setProperty("--guessColour", guess.value);
+    colourDiv.appendChild(`
+<div class="results">
+    <p>Your colour: </p>
+    <div id="guessColour"></div>
+</div>
+<p>Answer: ${colour}<p>
+<p>Colour difference: ${colourDifference(guess.value, colour)}
+    `);
+}
+
+function retry() {
     colourGen();
     guess.value = "";
 }
@@ -81,3 +92,4 @@ function colourDifference(hex1, hex2) {
 
 window.onload = load();
 guess = document.getElementById("guess");
+colourDiv = document.getElementById("colour")
